e09a057f1d7b9a9ccc87226cce6e9aa7ace46890
MuhammadChandraDarmawan <mchandradrmwn12@gmail.com>
2021-09-20 00:08:22 -0700

    Latihan Contoh2 dan file projek

---
+++ Contoh2.php
@@ -0,0 +1,12 @@
+<?php 
+{
+    { 
+        echo "<h1>Perkenalkan</h1>"; 
+        echo"<h1>Nama</h1>,;saya Muhammad Chandra Darmawan
+            saya mahasiswa UBSI
+            Jurusan Sistem Informasi Semester 3 
+            Tempat Tinggal saya di jalan Raya Wangun Kp. Lebaksari Rt02/Rw06
+            Kel.Sindang Sari Bogor Timur
+            Hobby Saya senang menggambar dan menonton film"; 
+    } 
+}
